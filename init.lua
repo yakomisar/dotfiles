de54ca81eb498b7f5001f4ec82414794316b7277
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2023-07-14 17:33:12 +0300

    disable arrow buttons

--- init.lua
+++ init.lua
@@ -15,3 +15,9 @@ vim.api.nvim_set_keymap('n', 'q', '<Nop>', {noremap = true})
 vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", {nocombine = true, fg="none"})
 vim.api.nvim_set_hl(0, "IndentBlanklineContextStart", {nocombine = false, underline=false, special="none"})
 
+-- Disable arrow buttons on the keyboard
+vim.api.nvim_set_keymap('n', '<Left>', ':echoerr "Use h"<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', '<Right>', ':echoerr "Use l"<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', '<Up>', ':echoerr "Use k"<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', '<Down>', ':echoerr "Use j"<CR>', {noremap = true, silent = false})
+
