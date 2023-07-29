a997c309868779a8a24babf88d8b1be27cc8eafc
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2023-07-30 00:32:00 +0300

    added CursorLineNr modifications

--- chadrc.lua
+++ chadrc.lua
@@ -1,16 +1,23 @@
- local M = {}
-     M.ui = {
-        theme = 'vscode_dark',
-        tabufline = {
-            overriden_modules = function()
-                return {
-                    buttons = function()
-                    return ""
-                    end,
-                }
-            end,
-        },
-    }
-     M.plugins = "custom.plugins"
-     M.mappings = require "custom.mappings"
- return M
+local M = {}
+M.ui = {
+  theme = "monekai",
+  tabufline = {
+    overriden_modules = function(modules)
+      table.remove(modules, 4)
+    end,
+  },
+  hl_override = {
+    Comment = {
+      italic = true,
+      fg = "#706b6b",
+    },
+    CursorLineNr = {
+      italic = true,
+      bold = true,
+      fg = "#e5c07b",
+    },
+  },
+}
+M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
+return M
