296a59d29e110312d4bdafe8fc53b5ab6d96213d
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2023-07-31 19:42:01 +0300

    style customization

--- chadrc.lua
+++ chadrc.lua
@@ -17,18 +17,37 @@ M.ui = {
         -- base05 - Default Foreground, Caret, Delimiters, Operators
         -- base06 - Light Foreground (Not often used)
         -- base07 - Light Background (Not often used)
-        -- base08 - Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
-        base09 = "#c6c0f8", --Integers, Boolean, Constants, XML Attributes, Markup Link Url
-        -- base0A - Classes, Markup Bold, Search Text Background
-        base0B = "#fddc6b", --Strings, Inherited Class, Markup Code, Diff Inserted
+        base08 = "white", -- Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+        base09 = "#c7c1f6", --Integers, Boolean, Constants, XML Attributes, Markup Link Url
+        base0A = "#8ae8f1", -- Classes, Markup Bold, Search Text Background
+        base0B = "#fddd69", --Strings, Inherited Class, Markup Code, Diff Inserted
         -- base0C - Support, Regular Expressions, Escape Characters, Markup Quotes
-        base0D = "#fe5b8b", --Functions, Methods, Attribute IDs, Headings
-        -- base0E - Keywords, Storage, Selector, Markup Italic, Diff Changed
-        -- base0F - Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
+        base0D = "#c0ef7f", --Functions, Methods, Attribute IDs, Headings
+        -- base0E = "red", -- Keywords, Storage, Selector, Markup Italic, Diff Changed
+        base0F = "#d699b6", -- Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
       },
       base_30 = {
         -- red = "red",
         -- white = "#e96366",
+      },
+      polish_hl = {
+        ["@function"] = { bold = true }, -- Function names
+        -- ["@function.call"] = { bold = true },
+        ["@number"] = { fg = "purple" },
+        ["@operator"] = {
+          fg = "#fc5e89",
+          bold = true,
+        }, -- operators like = != := ==
+        ["@keyword.return"] = {
+          fg = "#fcdd71",
+        },
+        ["@keyword.function"] = {
+          fg = "#fc5e89",
+          -- bold = true,
+        },
+        -- ["@variable"] = { bold = true }, -- Переменные (var/const)
+        Error = { fg = "red", bold = true },
+        Type = { bold = true }, -- тип данных (string/int..)
       },
     },
   },
@@ -41,6 +60,18 @@ M.ui = {
       italic = true,
       bold = true,
       fg = "#fedc67",
+    },
+    Number = {
+      bold = true,
+    },
+    Float = {
+      bold = true,
+    },
+    Boolean = {
+      bold = true,
+    },
+    Operator = {
+      italic = true,
     },
   },
 }
