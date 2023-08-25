af2d87394101ce482f4e4bdd2c83e1e9e66dfa42
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2023-08-25 23:35:59 +0300

    pre

--- nvim/lua/treesitter-config/init.lua
+++ nvim/lua/treesitter-config/init.lua
@@ -1,34 +1,37 @@
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	-- underline = true,
-	-- update_in_insert = true,
-	underline = false,
-	update_in_insert = false,
-	virtual_text = {
-		spacing = 2,
-		severity_limit = "Warning",
-	},
+  -- underline = true,
+  -- update_in_insert = true,
+  underline = false,
+  update_in_insert = false,
+  virtual_text = {
+    spacing = 2,
+    severity_limit = "Warning",
+  },
 })
+
 require("nvim-treesitter.configs").setup({
-	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = {
-		"c",
-		"lua",
-		"vim",
-		"go",
-		"vimdoc",
-		"query",
-		"html",
-		"javascript",
-		"go",
-		"gomod",
-		"gowork",
-		"gosum",
-	},
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	autotag = {
-		enable = true,
-	},
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = {
+    "c",
+    "lua",
+    "vim",
+    "go",
+    "vimdoc",
+    "query",
+    "html",
+    "javascript",
+    "go",
+    "gomod",
+    "gowork",
+    "gosum",
+    "sql",
+  },
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+    additional_vim_regex_highlighting = false,
+  },
+  autotag = {
+    enable = true,
+  },
 })
