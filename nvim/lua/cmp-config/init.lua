2751e05e0816b8652941bf009e9bf023ddea4578
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2023-09-03 19:09:23 +0300

    add some updates

--- nvim/lua/cmp-config/init.lua
+++ nvim/lua/cmp-config/init.lua
@@ -1,19 +1,19 @@
 -- import nvim-cmp plugin safely
 local cmp_status, cmp = pcall(require, "cmp")
 if not cmp_status then
-	return
+  return
 end
 
 -- import luasnip plugin safely
 local luasnip_status, luasnip = pcall(require, "luasnip")
 if not luasnip_status then
-	return
+  return
 end
 
 -- import lspkind plugin safely
 local lspkind_status, lspkind = pcall(require, "lspkind")
 if not lspkind_status then
-	return
+  return
 end
 
 -- local cmp = require('cmp')
@@ -25,57 +25,57 @@ luasnip.config.setup({})
 vim.opt.completeopt = "menu,menuone"
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		scrollbar = false,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-j>"] = cmp.mapping.select_next_item(),
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete({}),
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.locally_jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	}),
-	sources = {
-		{ name = "path" }, -- file system paths
-		{ name = "nvim_lsp", keyword_length = 1 }, -- lsp
-		{ name = "luasnip", keyword_length = 3 }, -- snippets
-		{ name = "buffer", keyword_length = 2 }, -- text within current buffer
-	},
-	-- configure lspkind for vs-code like icons
-	formatting = {
-		format = lspkind.cmp_format({
-			-- mode = "symbol", -- show only symbol annotations
-			mode = "symbol_text",
-			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-		}),
-	},
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    scrollbar = false,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete({}),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = {
+    { name = "path" },                       -- file system paths
+    { name = "nvim_lsp", keyword_length = 1 }, -- lsp
+    { name = "luasnip",  keyword_length = 3 }, -- snippets
+    { name = "buffer",   keyword_length = 2 }, -- text within current buffer
+  },
+  -- configure lspkind for vs-code like icons
+  formatting = {
+    format = lspkind.cmp_format({
+      -- mode = "symbol", -- show only symbol annotations
+      mode = "symbol_text",
+      maxwidth = 50,      -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+    }),
+  },
 })
--- nvim/lua/lsp-config/init.lua
+++ nvim/lua/lsp-config/init.lua
@@ -1,32 +1,27 @@
+-- local on_attach = function(_, bufnr)
+-- 	local bufmap = function(keys, func)
+-- 		vim.keymap.set("n", keys, func, { buffer = bufnr })
+-- 	end
 local on_attach = function(_, bufnr)
-	local bufmap = function(keys, func)
-		vim.keymap.set("n", keys, func, { buffer = bufnr })
-	end
+  local bufmap = function(keys, func)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", keys, func, { noremap = true, silent = true })
+  end
 
-	bufmap("<leader>r", vim.lsp.buf.rename)
-	bufmap("<leader>a", vim.lsp.buf.code_action)
-	bufmap("gd", vim.lsp.buf.definition)
-	bufmap("K", vim.lsp.buf.hover)
-	bufmap("gD", vim.lsp.buf.declaration)
-	bufmap("gI", vim.lsp.buf.implementation)
-	bufmap("<leader>D", vim.lsp.buf.type_definition)
-	bufmap("<leader>gr", vim.lsp.buf.references)
+  bufmap("<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>")
+  bufmap("<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+  bufmap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  bufmap("K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  bufmap("gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+  bufmap("gI", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+  bufmap("<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  bufmap("<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+  bufmap("<leader>q", "<cmd>Telescope diagnostics bufnr=0<CR>")
+  bufmap("[d", vim.diagnostic.goto_prev) -- jump to previous diagnostic in buffer
+  bufmap("]d", vim.diagnostic.goto_next) -- jump to next diagnostic in buffer
 
-	-- bufmap("gr", require("telescope.builtin").lsp_references)
-	-- bufmap("<leader>s", require("telescope.builtin").lsp_document_symbols)
-	-- bufmap("<leader>S", require("telescope.builtin").lsp_dynamic_workspace_symbols)
-
-	--   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	--   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	--   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	--   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	--   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	--   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	--   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-
-	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-		vim.lsp.buf.format()
-	end, {})
+  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+    vim.lsp.buf.format()
+  end, {})
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -35,83 +30,83 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 -- Mason
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		-- Replace these with whatever servers you want to install
-		"tsserver",
-		"gopls",
-		"lua_ls",
-	},
+  ensure_installed = {
+    -- Replace these with whatever servers you want to install
+    "tsserver",
+    "gopls",
+    "lua_ls",
+  },
 })
 
 local util = require("lspconfig/util")
 require("mason-lspconfig").setup_handlers({
 
-	function(server_name)
-		require("lspconfig")[server_name].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-	end,
+  function(server_name)
+    require("lspconfig")[server_name].setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
+  end,
 
-	["lua_ls"] = function()
-		require("lspconfig").lua_ls.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			single_file_support = true,
-			flags = {
-				debounce_text_changes = 150,
-			},
-			Lua = {
-				workspace = { checkThirdParty = false },
-				telemetry = { enable = false },
-			},
-		})
-	end,
+  ["lua_ls"] = function()
+    require("lspconfig").lua_ls.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+      single_file_support = true,
+      flags = {
+        debounce_text_changes = 150,
+      },
+      Lua = {
+        workspace = { checkThirdParty = false },
+        telemetry = { enable = false },
+      },
+    })
+  end,
 
-	["gopls"] = function()
-		require("lspconfig").gopls.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			cmd = { "gopls" },
-			filetypes = { "go", "gomod", "gowork", "gotmpl" },
-			root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-			settings = {
-				gopls = {
-					completeUnimported = true,
-					usePlaceholders = true,
-					analyses = {
-						unusedparams = true,
-					},
-				},
-			},
-		})
-	end,
+  ["gopls"] = function()
+    require("lspconfig").gopls.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+      cmd = { "gopls" },
+      filetypes = { "go", "gomod", "gowork", "gotmpl" },
+      root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+      settings = {
+        gopls = {
+          completeUnimported = true,
+          usePlaceholders = true,
+          analyses = {
+            unusedparams = true,
+          },
+        },
+      },
+    })
+  end,
 
-	["tsserver"] = function()
-		require("lspconfig").tsserver.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-	end,
+  ["tsserver"] = function()
+    require("lspconfig").tsserver.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
+  end,
 
-	["html"] = function()
-		require("lspconfig").html.setup({
-			filetypes = { "html", "css" },
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-	end,
+  ["html"] = function()
+    require("lspconfig").html.setup({
+      filetypes = { "html", "css" },
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
+  end,
 
-	-- another example
-	-- ["omnisharp"] = function()
-	--     require('lspconfig').omnisharp.setup {
-	--         filetypes = { "cs", "vb" },
-	--         root_dir = require('lspconfig').util.root_pattern("*.csproj", "*.sln"),
-	--         on_attach = on_attach,
-	--         capabilities = capabilities,
-	--         enable_roslyn_analyzers = true,
-	--         analyze_open_documents_only = true,
-	--         enable_import_completion = true,
-	--     }
-	-- end,
+  -- another example
+  -- ["omnisharp"] = function()
+  --     require('lspconfig').omnisharp.setup {
+  --         filetypes = { "cs", "vb" },
+  --         root_dir = require('lspconfig').util.root_pattern("*.csproj", "*.sln"),
+  --         on_attach = on_attach,
+  --         capabilities = capabilities,
+  --         enable_roslyn_analyzers = true,
+  --         analyze_open_documents_only = true,
+  --         enable_import_completion = true,
+  --     }
+  -- end,
 })
