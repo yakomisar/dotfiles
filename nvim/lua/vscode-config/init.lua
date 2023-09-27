c1d5487ecf4d10b15c8bb6ad507577aebd8a146e
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2023-09-27 19:46:17 +0300

    added helix config

--- nvim/lua/vscode-config/init.lua
+++ nvim/lua/vscode-config/init.lua
@@ -4,14 +4,7 @@ vim.o.background = "dark"
 -- For light theme
 -- vim.o.background = 'light'
 
-local c = require("vscode.colors").get_colors()
 require("vscode").setup({
-	-- -- Alternatively set style in setup
-	-- -- style = 'light'
-	--
-	-- -- Enable transparent background
-	-- transparent = true,
-	--
 	-- Enable italic comment
 	italic_comments = true,
 	-- Override colors (see ./lua/vscode/colors.lua)
@@ -19,20 +12,5 @@ require("vscode").setup({
 		vscGreen = "#505050",
 		vscPopupFront = "#FFFFFF",
 	},
-	--
-	-- -- Disable nvim-tree background color
-	-- disable_nvimtree_bg = true,
-	--
-	-- -- Override colors (see ./lua/vscode/colors.lua)
-	-- color_overrides = {
-	-- 	vscLineNumber = "#FFFFFF",
-	-- },
-	--
-	-- -- Override highlight groups (see ./lua/vscode/theme.lua)
-	-- group_overrides = {
-	-- 	-- this supports the same val table as vim.api.nvim_set_hl
-	-- 	-- use colors from this colorscheme by requiring vscode.colors!
-	-- 	Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-	-- },
 })
 require("vscode").load()
