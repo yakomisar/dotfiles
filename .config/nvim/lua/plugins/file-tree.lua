d852bfa54a56510af06def469f9868600589b14b
oleg.komisarenko <oleg.komisarenko@aliexpress.ru>
2024-08-18 23:57:44 +0300

    11/57

--- .config/nvim/lua/plugins/file-tree.lua
+++ .config/nvim/lua/plugins/file-tree.lua
@@ -1,43 +1,78 @@
 return {
-	"stevearc/oil.nvim",
-	event = "VeryLazy",
-	opts = {},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup({
-			default_file_explorer = true,
-			delete_to_trash = true,
-			skip_confirm_for_simple_edits = true,
-			view_options = {
-				show_hidden = true,
-				natural_order = true,
-				is_always_hidden = function(name, _)
-					return name == ".." or name == ".git"
-				end,
-			},
-			float = {
-				enabled = true,
-				padding = 2,
-				max_width = 90,
-				max_height = 30,
-				border = "rounded",
-			},
-			win_options = {
-				wrap = true,
-				winblend = 0,
-			},
-			keymaps = {
-				["<C-c>"] = false,
-				["q"] = "actions.close",
-				["<ESC>"] = "actions.close",
-			},
-		})
-		-- Open parent directory in floating window
-		vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+	{
+		"stevearc/oil.nvim",
+		event = "VeryLazy",
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				default_file_explorer = true,
+				delete_to_trash = true,
+				skip_confirm_for_simple_edits = true,
+				view_options = {
+					show_hidden = true,
+					natural_order = true,
+					is_always_hidden = function(name, _)
+						return name == ".." or name == ".git"
+					end,
+				},
+				float = {
+					enabled = true,
+					padding = 2,
+					max_width = 90,
+					max_height = 30,
+					border = "rounded",
+				},
+				win_options = {
+					wrap = true,
+					winblend = 0,
+				},
+				keymaps = {
+					["<C-c>"] = false,
+					["q"] = "actions.close",
+					["<ESC>"] = "actions.close",
+				},
+			})
+			-- Open parent directory in floating window
+			vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
 
-		-- -- Open parent directory in floating window
-		-- vim.keymap.set("n", "<leader>e", require("oil").toggle_float)
-	end,
+			-- -- Open parent directory in floating window
+			-- vim.keymap.set("n", "<leader>e", require("oil").toggle_float)
+		end,
+	},
+
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		event = "VeryLazy",
+		branch = "v3.x",
+		cmd = "Neotree",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = true,
+				buffers = {
+					follow_current_file = { enabled = true },
+				},
+				filesystem = {
+					follow_current_file = { enabled = true },
+					filtered_items = {
+						hide_dotfiles = false,
+						hide_gitignored = false,
+						hide_by_name = {
+							"node_modules",
+						},
+						never_show = {
+							".DS_Store",
+							"thumbs.db",
+						},
+					},
+				},
+			})
+		end,
+	},
 }
 -- return {
 -- 	"nvim-neo-tree/neo-tree.nvim",
--- .zshrc
+++ .zshrc
@@ -125,17 +125,19 @@ alias ......="cd ../../../../.."
 
 # Git
 alias gc="git commit -m"
-alias gca="git commit -a -m"
+alias gs="git switch"
+alias gm="git merge"
+# alias gca="git commit -a -m"
 # alias gp="git push origin HEAD"
 # alias gpu="git pull origin"
 alias gst="git status"
 alias glog="git log --graph --topo-order --pretty='%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N' --abbrev-commit"
 # alias gdiff="git diff"
-# alias gco="git checkout"
-# alias gb='git branch'
+alias gco="git checkout"
+alias gb='git branch'
 # alias gba='git branch -a'
-# alias gadd='git add'
-# alias ga='git add -p'
+alias ga='git add'
+# alias gadp='git add -p'
 # alias gcoall='git checkout -- .'
 # alias gr='git remote'
 # alias gre='git reset'
