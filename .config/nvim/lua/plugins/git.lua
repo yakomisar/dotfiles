5ed7c5dcb0afdf14683cce6222894825470023ec
oleg.komisarenko <oleg.komisarenko@aliexpress.ru>
2025-02-16 18:25:26 +0300

    update

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,4 +1,26 @@
 return {
+    -- ============ NEOGIT & DIFFVIEW ============
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "sindrets/diffview.nvim", -- optional - Diff integration
+
+            -- -- Only one of these is needed.
+            -- "nvim-telescope/telescope.nvim", -- optional
+            -- "ibhagwan/fzf-lua",      -- optional
+            -- "echasnovski/mini.pick", -- optional
+        },
+        keys = {
+            -- Neogit
+            { "<leader>gg", "<cmd>Neogit<CR>",              desc = "Open Neogit" },
+            { "<leader>gd", "<cmd>DiffviewOpen<cr>",        desc = "Open Diffview" },
+            { "<leader>gD", "<cmd>DiffviewClose<cr>",       desc = "Close Diffview" },
+            -- Optional additional mappings:
+            { "<leader>gF", "<cmd>DiffviewFocusFiles<cr>",  desc = "Focus Diffview File Panel" },
+            { "<leader>gT", "<cmd>DiffviewToggleFiles<cr>", desc = "Toggle Diffview File Panel" },
+        },
+        config = true
+    },
     {
         "lewis6991/gitsigns.nvim",
         opts = {
@@ -38,12 +60,12 @@ return {
                 -- _map("n", "<leader>gr", gs.reset_hunk, { buffer = bufnr, desc = "[R]eset [H]unk" })
                 -- _map("n", "<leader>gR", gs.reset_buffer, { buffer = bufnr, desc = "[R]eset [b]uffer" })
                 _map("n", "<leader>gp", gs.preview_hunk, { buffer = bufnr, desc = "[P]review [H]unk" })
-                _map("n", "<leader>gd", gs.diffthis, { buffer = bufnr, desc = "[D]iff This" })
+                -- _map("n", "<leader>gd", gs.diffthis, { buffer = bufnr, desc = "[D]iff This" })
                 _map(
                     "n",
                     "<leader>gt",
                     gs.toggle_current_line_blame,
-                    { buffer = bufnr, desc = "[G]itsigns [l]ine [b]lame" }
+                    { buffer = bufnr, desc = "[T]oggle line blame" }
                 )
             end,
         },
