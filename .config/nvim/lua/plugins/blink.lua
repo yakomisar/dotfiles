38a95780f65d49bc31879f7bef648f0234b8fe8d
oleg.komisarenko <oleg.komisarenko@aliexpress.ru>
2025-03-09 12:22:20 +0300

    added blink cmp

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,138 @@
+return {
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+        "onsails/lspkind.nvim", -- vs-code like pictograms
+    },
+
+    -- use a release tag to download pre-built binaries
+    version = '*',
+    event = { "InsertEnter" },
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+
+        -- Disable cmdline - отключаем для командной строки
+        cmdline = {
+            enabled = false
+        },
+
+        completion = {
+            keyword = { range = 'full' },
+
+            -- Don't select by default, auto insert on selection
+            list = { selection = { preselect = true, auto_insert = true } },
+
+            menu = {
+                -- Don't automatically show the completion menu
+                auto_show = true,
+                border = "rounded",
+
+                -- nvim-cmp style menu
+                draw = {
+                    columns = {
+                        { "label",     "label_description", gap = 1 },
+                        { "kind_icon", "kind",              gap = 1 }
+                    },
+                }
+            },
+            -- Show documentation when selecting a completion item
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 0,
+                update_delay_ms = 10,
+                treesitter_highlighting = true,
+                window = { border = "rounded" },
+            },
+
+            -- Display a preview of the selected item on the current line
+            -- Показывает по сути placeholder того что будет показано
+            ghost_text = { enabled = false },
+        },
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
+        -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
+        -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+        --
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help
+        --
+        -- See the full "keymap" documentation for information on defining your own keymap.
+        keymap = {
+            -- preset = 'default'
+            ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+            ["<C-e>"] = { "hide", "fallback" },
+            ["<CR>"] = { "accept", "fallback" },
+            ["<Tab>"] = {
+                function(cmp)
+                    return cmp.select_next()
+                end,
+                "snippet_forward",
+                "fallback",
+            },
+            ["<S-Tab>"] = {
+                function(cmp)
+                    return cmp.select_prev()
+                end,
+                "snippet_backward",
+                "fallback",
+            },
+            ["<C-k>"] = { "select_prev", "fallback" },
+            ["<C-j>"] = { "select_next", "fallback" },
+            ["<C-p>"] = { "select_prev", "fallback" },
+            ["<C-n>"] = { "select_next", "fallback" },
+            ["<C-up>"] = { "scroll_documentation_up", "fallback" },
+            ["<C-down>"] = { "scroll_documentation_down", "fallback" },
+        },
+
+        appearance = {
+            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+            -- Useful for when your theme doesn't support blink.cmp
+            -- Will be removed in a future release
+            use_nvim_cmp_as_default = true,
+            -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- Adjusts spacing to ensure icons are aligned
+            nerd_font_variant = 'mono'
+        },
+
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = {
+            default = { 'lsp', 'snippets', 'buffer', 'path' },
+            per_filetype = {
+                sql = { 'snippets', 'dadbod', 'buffer' },
+            },
+            providers = {
+                dadbod = {
+                    name = 'Dadbod',
+                    module = 'vim_dadbod_completion.blink',
+                },
+                buffer = {
+                    min_keyword_length = 3,
+                    max_items = 5,
+                },
+            }
+        },
+
+        -- Показывает переменную которая будет заполнена в функции
+        signature = {
+            enabled = true,
+            window = { border = "rounded" },
+        },
+
+        -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+        --
+        -- See the fuzzy documentation for more information
+        fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+}
