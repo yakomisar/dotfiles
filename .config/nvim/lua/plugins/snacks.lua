81d6f1361f01a361b29893184f9d88511f086ac2
oleg.komisarenko <oleg.komisarenko@aliexpress.ru>
2025-04-08 15:42:32 +0300

    update

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -37,13 +37,6 @@ return {
 			desc = "Grep",
 		},
 		{
-			"<leader>:",
-			function()
-				Snacks.picker.command_history()
-			end,
-			desc = "Command History",
-		},
-		{
 			"<leader>n",
 			function()
 				Snacks.picker.notifications()
